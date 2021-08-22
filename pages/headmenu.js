7a9b4c48ca95690a25b18355399994a09ed26078
ppoptimus <r.pumpo@gmail.com>
2021-08-22 21:44:45 +0700

    set meta

--- pages/headmenu.js
+++ pages/headmenu.js
@@ -1,25 +1,15 @@
-import React, { useState } from "react";
+
 import Link from "next/link";
 export default function Headmenu() {
-  const [active, setActive] = useState(false);
 
-  const handleClick = () => {
-    setActive(!active);
-  };
   return (
     <>
-      <nav className="flex justify-between items-center flex-wrap bg-gradient-to-r from-green-400 to-blue-500 p-3 ">
+      <div className="flex justify-between items-center flex-wrap bg-gradient-to-r from-green-400 to-blue-500 p-3 ">
         <Link href="/">
           <a href="#" className="flex items-center py-0 px-1">
-            <img src="img/logo.png" alt="Logo" className="" />
+            <img src="img/logo.png" width={182} height={57} alt="pg slot" />
           </a>
         </Link>
-
-        {/* <button className=" inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none" onClick={handleClick}>
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </button> */}
         
         <a href="/">
           <div className="mb-1 ml-1">
@@ -38,24 +28,7 @@ export default function Headmenu() {
             </button>
           </div>
         </a>
-
-        {/* <div className={`${active ? "" : "hidden"}   w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
-          <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
-            <Link href="/">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white ">หน้าหลัก</a>
-            </Link>
-            <Link href="/">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">ดาวน์โหลด</a>
-            </Link>
-            <Link href="/">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">ติดต่อเรา</a>
-            </Link>
-            <a href="/" className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">
-              สมัครสมาชิก
-            </a>
-          </div>
-        </div> */}
-      </nav>
+      </div>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,40 @@
-import Head from 'next/head'
-import Card from './card';
-import Footmobile from './footmobile';
-import HeadMenu from './headmenu';
+import Head from "next/head";
+import Card from "./card";
+import Desk from "./desk";
+import Footmobile from "./footmobile";
+import HeadMenu from "./headmenu";
 
 export default function Home() {
   return (
     <div className="bg-gradient-to-r from-green-400 to-blue-500">
-        <Head>
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Play Slot Online</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <HeadMenu />
+      <Head>
+        <meta httpEquiv="content-language" content="th" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Play Slot Online</title>
+        <meta
+          name="description"
+          content="PG SLOT Online เปิดตลอด 24 ชั่วโมง, ระบบเติมเงินแบบเรียลไทม์, Auto deposit ระบบฝากถอนอัตโนมัติ, user เดียวเล่นได้ทุกเกม, สล็อตออนไลน์บนมือถือ, ได้เงินจริงมีฟรีเครดิต, แตกทีเป็นแสน"
+        />
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <HeadMenu />
       <div className=" items-center justify-center min-h-screen py-1">
-        <Card/>
+        <Card />
+        <Desk />
       </div>
-        <Footmobile/>
-        <footer className="flex items-center justify-center w-full bottom-2">
-          <a
-            className="flex items-center justify-center"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by <img src="img/logo1.png" alt="pg slot" width={100} height={100} />
-          </a>
-        </footer>
+      <Footmobile />
+      <footer className="flex items-center justify-center w-full bottom-2">
+        <a
+          className="flex items-center justify-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by <img src="img/logo1.png" alt="pg slot" width={100} height={100} />
+        </a>
+      </footer>
     </div>
   );
 }
